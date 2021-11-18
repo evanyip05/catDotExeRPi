325906288248507c87ccf3edf4137084c476d7c6
Evan Yip <92695816+evanyip05@users.noreply.github.com>
2021-11-17 17:57:05 -0800

    Update catDotExe.py

--- catDotExe.py
+++ catDotExe.py
@@ -1,7 +1,14 @@
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
+from time import sleep
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(8, GPIO.OUT)
 i = 0
 
 while i == 0:
-    GPIO.output(8, GPIO.HIGH)
+    GPIO.output(8, 1)
+    sleep(1)
+    GPIO.output(8, 0)
+    sleep(1)
+    i += 1
+
+GPIO.cleanup()
